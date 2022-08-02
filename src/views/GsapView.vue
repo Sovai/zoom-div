e4787c50c0b1bb0542914a4ed010d9b30fe108b7
KEAN Sovai <kean.sovai@ababank.com>
2022-08-02 10:29:10 +0700

    test pinch

--- src/views/GsapView.vue
+++ src/views/GsapView.vue
@@ -16,26 +16,50 @@
 <script setup>
 import { nextTick, onMounted, ref } from "vue";
 import { gsap, Power3 } from "gsap";
+import Hammer from "hammerjs";
+
 import Draggable from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 let selected = ref(null);
 let scale = ref(1);
 
+let instance = ref(null);
+let lastScale = ref(null);
 onMounted(async () => {
   await nextTick();
+  instance.value = new Hammer(document.getElementById("wrapper"), {});
+  instance.value.get("pinch").set({
+    enable: true,
+  });
+
   Draggable.create("#wrapper", {
     onDragStart: function () {
       gsap.killTweensOf("#wrapper");
     },
     bounds: document.getElementById("container"),
   });
+
+  instance.value.on("tap doubletap pan pinch panend pinchend", function (ev) {
+    if (ev.type === "pinch") {
+      scale.value = Math.max(0.999, Math.min(lastScale.value * ev.scale, 4));
+    }
+    if (ev.type === "pinchend") {
+      lastScale.value = scale.value;
+    }
+
+    gsap.to("#wrapper", {
+      scale: scale.value,
+      duration: 1,
+      ease: Power3.easeInOut,
+    });
+  });
 });
 
 function clicked(e, i) {
   selected.value = selected.value === i ? null : i;
-  // scale.value = scale.value === 1 ? 1.5 : 1;
-  scale.value = 1.5;
+
+  scale.value = selected.value ? 1.5 : 1;
   let rect = e.target.getBoundingClientRect();
   let centerX = window.innerWidth / 2;
   let centerY = window.innerHeight / 2;
